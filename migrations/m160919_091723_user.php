4b80b5910e25ff1f57973e8544288fbb38458196
thinfell · 尹 <thinfell@qq.com>
2016-09-19 18:19:22 +0800

    user表编码与默认值修正

--- migrations/m160919_091723_user.php
+++ migrations/m160919_091723_user.php
@@ -8,18 +8,18 @@ class m160919_091723_user extends Migration
     {
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB COMMENT="用户表"';
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB COMMENT="用户表"';
         }
 
         $this->createTable('user', [
-            'id' => $this->primaryKey()->comment('用户ID'),
-            'name' => $this->string()->notNull()->comment('用户名'),
-            'avatar' => $this->string()->comment('头像'),
-            'mobile' => $this->string(11)->notNull()->comment('手机号'),
-            'email' => $this->string()->defaultValue('')->comment('邮箱'),
-            'password_hash' => $this->string()->notNull()->comment('密码'),
+            'id' => $this->primaryKey()->comment('用户编号'),
+            'name' => $this->string()->unique()->notNull()->comment('用户名'),
+            'avatar' => $this->string()->defaultValue('')->comment('头像'),
+            'mobile' => $this->string(11)->unique()->notNull()->comment('手机号'),
+            'email' => $this->string()->unique()->defaultValue('')->comment('邮箱'),
+            'password_hash' => $this->string()->defaultValue('')->comment('密码'),
             'status' => $this->smallInteger(6)->defaultValue(1)->comment('状态'),
-            'auth_key' => $this->string(32),
+            'auth_key' => $this->string(32)->defaultValue('')->comment('Auth Key'),
         ], $tableOptions);
     }
 
