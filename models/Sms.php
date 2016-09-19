5fec1f6758e914ae650187d08c9ba20b7fda6221
thinfell · 尹 <thinfell@qq.com>
2016-09-19 18:19:55 +0800

    sms类rules完善

--- models/Sms.php
+++ models/Sms.php
@@ -28,11 +28,12 @@ class Sms extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['mobile'], 'required'],
-            [['sms', 'send_time', 'action'], 'safe'],
+            [['mobile', 'sms', 'send_time', 'action'], 'required'],
             [['send_time'], 'integer'],
+            [['mobile'], 'string', 'max' => 11],
             [['mobile'], 'match', 'pattern' => '/^1[34578]{1}\d{9}$/', 'message' => '手机号格式不正确'],
             [['sms'], 'string', 'max' => 6],
+            [['action'], 'string', 'max' => 255],
         ];
     }
 
