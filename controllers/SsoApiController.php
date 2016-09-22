a1b44c3758ab50d05cf3a0dea7c498f989477cd2
thinfell · 尹 <thinfell@qq.com>
2016-09-22 16:03:02 +0800

    新增注册验证API

--- controllers/SsoApiController.php
+++ controllers/SsoApiController.php
@@ -7,6 +7,7 @@ use app\models\Tickit;
 use yii\web\Controller;
 use app\models\Website;
 use yii\web\Response;
+use app\models\User;
 
 class SsoApiController extends Controller
 {
@@ -55,6 +56,43 @@ class SsoApiController extends Controller
         return [
             'code' => 200,
             'message' => $tickit->user_id,
+        ];
+    }
+
+    public function actionRegister()
+    {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+        $sso_website_id = Yii::$app->request->get('sso_website_id');
+        $website = Website::findOne($sso_website_id);
+        if(!isset($website->secret)){
+            return [
+                'code' => 0,
+                'message' => 'sso_website_id error',
+            ];
+        }
+        $sso_website_secret = $website->secret;
+        $sign = Yii::$app->request->get('sign');
+        $timestamp = Yii::$app->request->get('timestamp');
+        if(md5($sso_website_id.$sso_website_secret.$timestamp) != $sign){
+            return [
+                'code' => 0,
+                'message' => 'sign error',
+            ];
+        }
+        $AuthenTickitRequestParamName = Yii::$app->request->get('AuthenTickitRequestParamName');
+        $tickit = Tickit::find()->where(['value' => $AuthenTickitRequestParamName])->one();
+        if(!isset($tickit->user_id)){
+            return [
+                'code' => 0,
+                'message' => 'AuthenTickitRequestParamName error',
+            ];
+        }
+        Tickit::deleteAll(['value' => $AuthenTickitRequestParamName]);
+        $user = User::getUserByUserid($tickit->user_id);
+        Yii::$app->user->login($user, 0);
+        return [
+            'code' => 200,
+            'message' => $user,
         ];
     }
 
